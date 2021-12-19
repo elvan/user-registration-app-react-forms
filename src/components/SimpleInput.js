9f32b441c7982ed6e5a47d89f6c8159b1d79532a
Elvan H <elvanhd@gmail.com>
2021-12-20 04:59:43 +0700

    adding basic validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -11,6 +11,10 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
+    if (enteredName.trim() === '') {
+      return;
+    }
+
     console.log(enteredName);
 
     const enteredValue = nameInputRef.current.value;
