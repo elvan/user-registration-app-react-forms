01544261bbb32a56db31f27d51e3b9503f8eac9c
Elvan Hidayat <elvanhd@gmail.com>
2024-06-16 22:26:09 +0700

    Add submit handler to Login component
    - Implement handleSubmit function to prevent default form submission
    - Add onSubmit attribute to form element to call handleSubmit function
    - Include console log to indicate form submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,11 @@
 export default function Login() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('Submitted!');
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className='control-row'>
